2fe945be26ab2d52636021dde42e8672b9fb4eaf
nayeong kim <psi505968@gmail.com>
2024-07-18 17:22:02 +0900

    first commit

---
+++ src/main/java/com/server/bearmurderermulti/BearMurdererMultiApplication.java
@@ -0,0 +1,13 @@
+package com.server.bearmurderermulti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BearMurdererMultiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BearMurdererMultiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/server/bearmurderermulti/BearMurdererMultiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.server.bearmurderermulti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BearMurdererMultiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
