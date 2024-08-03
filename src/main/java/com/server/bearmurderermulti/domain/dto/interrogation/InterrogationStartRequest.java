2ae373d85856f4782bd0f1844aae253189b0123c
kny3037 <psi505968@gmail.com>
2024-08-03 15:07:20 +0900

    [Feat] : Implement interrogation start communication

---
+++ src/main/java/com/server/bearmurderermulti/domain/dto/interrogation/InterrogationStartRequest.java
@@ -0,0 +1,29 @@
+package com.server.bearmurderermulti.domain.dto.interrogation;
+
+import com.server.bearmurderermulti.domain.entity.GameSet;
+import com.server.bearmurderermulti.domain.entity.Interrogation;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class InterrogationStartRequest {
+
+    private Long gameSetNo;
+    private String npcName;
+    private String weapon;
+
+    public Interrogation toEntity(GameSet gameSet) {
+        return Interrogation.builder()
+                .npcName(this.npcName)
+                .weapon(this.weapon)
+                .gameSet(gameSet)
+                .userQuestion(null)
+                .answer(null)
+                .heartRate(null)
+                .build();
+    }
+
+}
