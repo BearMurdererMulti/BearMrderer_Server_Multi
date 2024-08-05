2b5c102b83918dcabfcfbbb341dc46f581bb7504
kny3037 <psi505968@gmail.com>
2024-08-05 21:21:58 +0900

    [Feat] : End credit letters response and handling implemented

---
+++ src/main/java/com/server/bearmurderermulti/domain/dto/game/SurvivorLetter.java
@@ -0,0 +1,17 @@
+package com.server.bearmurderermulti.domain.dto.game;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class SurvivorLetter {
+
+    private String name;
+    private Letter letter;
+
+}
