a0c6fd2ea042d9426fe0ef4f967f25f75c4d99c6
kny3037 <psi505968@gmail.com>
2024-08-06 18:41:04 +0900

    [BugFix] : Add dual-member support to GameSet entity

---
+++ src/main/java/com/server/bearmurderermulti/domain/dto/game/StartGameRequest.java
@@ -0,0 +1,15 @@
+package com.server.bearmurderermulti.domain.dto.game;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class StartGameRequest {
+
+    private String participantNickname;
+}
